cede5fae959823dd206a509a70d032907f8e9820
Tbdtonigut <tbdtonigut@gmail.com>
2020-04-03 16:11:17 +0200

    Segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:english_words/english_words.dart';
+import 'package:flutte_project/pages/homePage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    printJson();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bienvenido a esta pedazo de app'),
-        ),
-        body: Center(
-          child: Text('Hola Mundo, soy toni'),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: News(),
     );
+  }
+}
+
+void printJson() async {
+  final response = await http.get('https://pokeapi.co/api/v2/pokemon');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    print(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Error');
   }
 }
---
+++ lib/pages/homePage.dart
@@ -0,0 +1,48 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewsState extends State<News> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Add from here...
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+}
+
+class News extends StatefulWidget {
+  @override
+  NewsState createState() => NewsState();
+}
